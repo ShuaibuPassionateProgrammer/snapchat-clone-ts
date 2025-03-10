a1d1ee95476a0850d8a19f99a1fa5d9eb72e503f
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-03-10 21:22:19 +0100

    Initial commit

---
+++ app/chat/[id]/error.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+cosnt Error = () => {
+    return (
+        <div>
+            <h1>Error Page</h1>
+        </div>
+    );
+};
+
+export default Error;
---
+++ app/chat/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ChatHistoryPage = () => {
+    return (
+        <div>
+            <h1>Chat History Page</h1>
+        </div>
+    );
+};
+
+export default ChatHistoryPage;
