8c389e48531d9f3eecfd2b4d31152ca25329fe8a
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-03-16 22:15:09 +0100

    Working on Chat

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,11 +1,17 @@
-import React from "react";
+import ChatCamera from "@/components/chat/chat-camera";
+import Image from "next/image";
 
-const ChatRootPage = () => {
+const ChatRootPage = async () => {
     return (
-        <div>
-            <h1>Chat Root Page</h1>
-        </div>
-    );
+		<main className='flex-grow bg-sigMain items-center flex px-2'>
+			<div className='bg-chat bg-right-bottom rounded-3xl w-full h-[96%] flex items-center justify-center px-6'>
+				<ChatCamera />
+				<div className='hidden lg:block'>
+					<Image src={"/snapemoji.png"} width={500} height={600} alt='Snap avatar' />
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default ChatRootPage;
