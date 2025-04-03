9d2c60d83e84e0d2259fb554d094fc2d194ee758
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-04-03 18:01:12 +0100

    Create Navbar shared component

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -11,6 +11,13 @@ const Navbar = () => {
     return (
         <header className='w-full py-4 px-8 flex justify-between items-center'>
             <Image src='/logo.svg' width={40} height={40} alt='Snapchat logo' className='cursor-pointer' />
+            <div className='flex space-x-1'>
+                <Button className='bg-transparent hover:bg-primary/5 text-black'>Stories</Button>
+				<Button className='bg-transparent hover:bg-primary/5 text-black'>Spotlight</Button>
+				<Button asChild className='bg-transparent hover:bg-primary/5 text-black'>
+					<Link href={"/chat"}> Chat</Link>
+				</Button>
+            </div>
         </header>
     );
 };
