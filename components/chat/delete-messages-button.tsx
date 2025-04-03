90243650850043f6d22d78fbe7c2022cdcc449be
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-04-03 18:25:37 +0100

    Create Delete Message button

---
+++ components/chat/delete-messages-button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const DeleteMessagesButton = () => {
+	
+	return (
+		<div>
+			<h1>Delete Messages Button</h1>
+		</div>
+	);
+};
+
+export default DeleteMessagesButton;
