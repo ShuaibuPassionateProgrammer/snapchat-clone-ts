9887aeb2f6924c07effca6de3293b7c88126929a
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-04-03 17:58:53 +0100

    Create Navbar shared component

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,4 +1,9 @@
+import Image from "next/image";
 import React from "react";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import LogoutButton from "./logout-button";
+import { auth } from "@/auth";
 
 const Navbar = () => {
     return (
